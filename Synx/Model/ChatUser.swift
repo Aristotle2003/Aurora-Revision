00b10ec93697b250b2a896911a51b8ab6dec1886
Mingyuan Shao <156740934+Aristotle2003@users.noreply.github.com>
2024-11-11 23:27:30 -0500

    Update ChatUser.swift

--- Synx/Model/ChatUser.swift
+++ Synx/Model/ChatUser.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Shawn on 10/17/24.
 //
+
 import Foundation
 
 struct ChatUser : Identifiable{
@@ -11,11 +12,13 @@ struct ChatUser : Identifiable{
     var id: String{uid}
     let uid, email, profileImageUrl: String
     let fcmToken: String
+    var hasPosted: Bool  // 新增属性
     
     init(data:[String: Any]){
         self.uid = data["uid"] as? String ?? ""
         self.email = data["email"] as? String ?? ""
         self.profileImageUrl = data["profileImageUrl"] as? String ?? ""
         self.fcmToken = data["fcmToken"] as? String ?? ""
+        self.hasPosted = data["hasPosted"] as? Bool ?? false  // 初始化属性
     }
 }
